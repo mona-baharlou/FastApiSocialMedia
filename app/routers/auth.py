080ad34e8c7121daf6a80f2a024cbcfdd7351690
mona-baharlou <mona.baharlou755@gmail.com>
2025-08-10 09:49:16 +0330

    login changed to use OAuth2PasswordRequestForm

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
 from sqlalchemy.orm import Session
 from .. import database, schemas, models, utils, oauth2
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter(
     tags=['authentication']
@@ -8,11 +9,11 @@ router = APIRouter(
 
 
 @router.post("/login")
-def login(user_credentials: schemas.UserLogin,
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(),
           db: Session = Depends(database.get_db)):
     
     user = db.query(models.User).filter(
-        models.User.email == user_credentials.email).first()
+        models.User.email == user_credentials.username).first()
 
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
